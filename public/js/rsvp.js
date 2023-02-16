ad0e122df767cce78ef045595664632a837d8d80
anelson92 <amanda92nelson@gmail.com>
2023-02-15 20:22:58 -0600

    added form updates

---
+++ public/js/rsvp.js
@@ -0,0 +1,13 @@
+const addEvent = document.getElementById()
+
+const rsvp = document.querySelector('rsvp').value;
+
+// listen to button on main.handlebars
+// once clicked add that event to list of events that the user has RSVP'd to 
+
+
+
+
+
+
+rsvpEvent.addEventListener('click', addEvent)
