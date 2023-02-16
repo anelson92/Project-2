c38dfef53018f355a5d3cdfc852fcc96e0fc55f2
Fox Stokes <foxvalentine001@gmail.com>
2023-02-15 19:06:30 -0600

    add render and /newEvent

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
+const { User, Event } = require('../models');
 const withAuth = require('../utils/auth');
-const { User, Event, Guest } = require('../models');
+
+//const { events } = require('inquirer/lib/utils/events');//
 
 router.get('/', async (req, res)=> {
 try {
@@ -14,11 +16,11 @@ try {
     });
   
       // Serialize data so the template can read it
-      const events = eventData.map((event) => event.get({ plain: true }));
+      const events = eventData.map((events) => events.get({ plain: true }));
   
       // Pass serialized data and session flag into template
       res.render('homepage', { 
-        projects, 
+        events, 
         logged_in: req.session.logged_in 
       });
     } catch (err) {
@@ -79,8 +81,28 @@ router.get('/login', (req, res) => {
     }
   });
 
-  router.get('/newEvent', async (req, res) => {
-    res.render('newEvent');
+  router.get('/newEvent', withAuth, async (req, res) => {
+    try {
+      const eventData = await Event.findByPk(req.params.id, {
+        include: [
+          {
+            model: User,
+            attributes: ['name'],
+          },
+        ],
+      });
+  
+      const events = eventData.map((event) => event.get({ plain: true }));
+    
+      res.render('newEvent', {
+        ...events,
+        logged_in: req.session.logged_in
+      });
+
+    } catch (err) {
+      res.status(500).json(err);
+    }
+   
   });
 
   module.exports = router;
